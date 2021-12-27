d92ce689188c193433a3dd3c0dedbbb3f0b6ca87
SamirRodrigues <emaildesamir@gmail.com>
2021-12-27 00:50:06 -0300

    Updates

---
+++ precache-manifest.2b5cbabbf8c11920376ebfb016fa2b0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e12bc7d1252c5626b8e93d5162d9d1",
+    "url": "/ShoppingCart/index.html"
+  },
+  {
+    "revision": "399de97395e7773fa674",
+    "url": "/ShoppingCart/static/css/main.35134b96.chunk.css"
+  },
+  {
+    "revision": "12f560e8129a457eb932",
+    "url": "/ShoppingCart/static/js/2.9b5b57e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShoppingCart/static/js/2.9b5b57e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399de97395e7773fa674",
+    "url": "/ShoppingCart/static/js/main.8b7a4de1.chunk.js"
+  },
+  {
+    "revision": "50a79f0348c5aa16f521",
+    "url": "/ShoppingCart/static/js/runtime-main.ded8f8e9.js"
+  }
+]);
